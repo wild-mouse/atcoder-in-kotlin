51297e93dfe2635d96522b71e83c62c3a2cf38d5
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-08-14 10:59:10 +0900

    Add Dodecagon

---
+++ src/kotlin/com/wildmouse/atcoder/contest/beginner/c134/Dodecagon.kt
@@ -0,0 +1,13 @@
+package com.wildmouse.atcoder.contest.beginner.c134
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    try {
+        val sc = Scanner(System.`in`)
+        val r = sc.nextInt()
+        println(3 * r * r)
+    } catch (e: Exception) {
+        println(e)
+    }
+}
